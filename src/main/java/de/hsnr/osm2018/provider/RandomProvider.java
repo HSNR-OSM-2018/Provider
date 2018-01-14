8f3908349da3fe18a2594d45f7fa83609d18c8b7
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-14 15:39:47 +0100

    Implemented random graph provider

--- src/main/java/de/hsnr/osm2018/provider/RandomProvider.java
+++ src/main/java/de/hsnr/osm2018/provider/RandomProvider.java
@@ -3,74 +3,62 @@ package de.hsnr.osm2018.provider;
 import de.hsnr.osm2018.data.data.FilteredDataProvider;
 import de.hsnr.osm2018.data.graph.EdgeType;
 import de.hsnr.osm2018.data.graph.Graph;
+import de.hsnr.osm2018.data.graph.Node;
 
-public class RandomProvider implements FilteredDataProvider {
+import java.util.Random;
 
-    private Graph mGraph;
+public class RandomProvider extends FilteredDataProvider {
 
-    public RandomProvider() {
+    private static final Double mLeft = 6.1702D;
+    private static final Double mTop = 51.4634D;
+    private static final Double mRight = 7.2482D;
+    private static final Double mBottom = 51.0794D;
+
+    public RandomProvider(int nodeSize) {
+        this(nodeSize, 4);
+    }
+
+    public RandomProvider(int nodeSize, int halfDensity) {
         this.mGraph = new Graph();
-        //TODO: fill graph
-    }
-
-    /**
-     * Get a subset of the full @{@link Graph} managed by this data-provider.
-     * The subset contains all relevant nodes and edges that are contained within a rectangular bound by the start and destination points.
-     * It is furthermore widen by the vicinityRage used to be able for including slight differences from the air line.
-     * Within the radius of the start- and destination positions edges of all @{@link EdgeType}s are included. The remaining area contains only edges of certain @{@link EdgeType}s (e.g. motorway, trunk and primary). Nodes without edges of these types will be omitted as well.
-     * Building this subset of the graph is based on the assumption that only within a certain range all types of roads are used for traveling. For traveling longer distances only motorways and other high-speed roads are used.
-     *
-     * @param startLatitude        Latitude of start position
-     * @param startLongitude       Longitude of start position
-     * @param destinationLatitude  Latitude of destination position
-     * @param destinationLongitude Longitude of destination position
-     * @param globalVicinityRange  Tolerance range that will widen the bounding box. value should be interpreted as degree
-     * @param localVicinityRadius  Subset local radius around start- and destination positions. value should be interpreted as degree
-     * @return Graph subset of graph with nodes and edges within the bound box
-     */
-    @Override
-    public Graph getGraph(double startLatitude, double startLongitude, double destinationLatitude, double destinationLongitude, double globalVicinityRange, double localVicinityRadius) {
-        return mGraph;
-    }
-
-    /**
-     * Get a subset of the full @{@link Graph} managed by this data-provider.
-     * The subset contains all nodes and edges that are contained within a rectangular bound by the start and destination points.
-     * It is furthermore widen by the vicinityRage used to be able for including slight differences from the air line.
-     *
-     * @param startLatitude        Latitude of start position
-     * @param startLongitude       Longitude of start position
-     * @param destinationLatitude  Latitude of destination position
-     * @param destinationLongitude Longitude of destination position
-     * @param vicinityRange        Tolerance range that will widen the bounding box. value should be interpreted as degree
-     * @return Graph subset of graph with nodes and edges within the bound box
-     */
-    @Override
-    public Graph getGraph(double startLatitude, double startLongitude, double destinationLatitude, double destinationLongitude, double vicinityRange) {
-        return mGraph;
-    }
-
-    /**
-     * Get a subset of the full @{@link Graph} managed by this data-provider.
-     * The subset contains all nodes and edges that are contained within an circle around the point. The circle radius is defined by the parameter vicinityRange.
-     *
-     * @param pointLatitude  Latitude of center point
-     * @param pointLongitude Longitude of center point
-     * @param vicinityRadius Subset radius. value should be interpreted as degree
-     * @return Graph subset of graph with nodes and edges within the bound circle
-     */
-    @Override
-    public Graph getGraph(double pointLatitude, double pointLongitude, double vicinityRadius) {
-        return mGraph;
-    }
-
-    /**
-     * Get the complete @{@link Graph} managed by this data-provider
-     *
-     * @return Graph
-     */
-    @Override
-    public Graph getGraph() {
-        return mGraph;
+        double minLatitude = Math.min(mTop, mBottom);
+        double rangeLatitude = Math.max(mTop, mBottom) - minLatitude;
+        double minLongitude = Math.min(mLeft, mRight);
+        double rangeLongitude = Math.max(mLeft, mRight) - minLongitude;
+        Random random = new Random();
+        for (int i = 0; i < nodeSize; i++) {
+            mGraph.add(new Node((long) i, minLatitude + rangeLatitude * random.nextDouble(), minLongitude + rangeLongitude * random.nextDouble()));
+        }
+        for (int start = 0; start < nodeSize; start++) {
+            int tempDensity = halfDensity / 2 + random.nextInt(halfDensity);
+            for (int j = 0; j < tempDensity; j++) {
+                Long dest;
+                do {
+                    dest = (long) random.nextInt(nodeSize);
+                } while (dest.intValue() == start);
+                int length = 10 * random.nextInt(5000);
+                short speed = (short) (10 * random.nextInt(13));
+                double typeRand = random.nextDouble();
+                EdgeType type;
+                if (typeRand < 0.1D) {
+                    type = EdgeType.MOTORWAY;
+                } else if (typeRand < 0.13D) {
+                    type = EdgeType.MOTORWAY_LINK;
+                } else if (typeRand < 0.28D) {
+                    type = EdgeType.TRUNK;
+                } else if (typeRand < 0.32D) {
+                    type = EdgeType.TRUNK_LINK;
+                } else if (typeRand < 0.52D) {
+                    type = EdgeType.PRIMARY;
+                } else if (typeRand < 0.55D) {
+                    type = EdgeType.PRIMARY_LINK;
+                } else {
+                    type = EdgeType.RESIDENTIAL;
+                }
+                mGraph.getNode((long) start).addEdge(dest, length, speed, type);
+                if (random.nextDouble() < 0.9D) { //Create reverse route for 90% of "roads"
+                    mGraph.getNode(dest).addEdge((long) start, length, speed, type);
+                }
+            }
+        }
     }
 }
