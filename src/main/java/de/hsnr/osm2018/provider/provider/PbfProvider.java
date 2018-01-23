a9eb11efa4d37788fc8819ff55f72667c7cc3852
Sebastian Bair <sebastian.bair@googlemail.com>
2018-01-23 12:26:36 +0100

    Adjusted Pbf-provider

--- src/main/java/de/hsnr/osm2018/provider/provider/PbfProvider.java
+++ src/main/java/de/hsnr/osm2018/provider/provider/PbfProvider.java
@@ -1,11 +1,12 @@
 package de.hsnr.osm2018.provider.provider;
 
 import de.hsnr.osm2018.data.data.FilteredDataProvider;
-import de.hsnr.osm2018.provider.pbfReader.PBFReader;
-import de.hsnr.osm2018.provider.pbfReader.PBFNodeSink;
+import de.hsnr.osm2018.data.graph.Graph;
+import de.hsnr.osm2018.data.graph.Node;
 import de.hsnr.osm2018.provider.pbfReader.complexReader.ComplexPbfReader;
 import de.hsnr.osm2018.provider.pbfReader.complexReader.InitSink;
-import de.hsnr.osm2018.provider.pbfReader.complexReader.NodeSink;
+
+import java.util.HashMap;
 
 public class PbfProvider extends FilteredDataProvider {
 
@@ -15,5 +16,6 @@ public class PbfProvider extends FilteredDataProvider {
         InitSink initsink = new InitSink();
         ComplexPbfReader reader = new ComplexPbfReader(fileName, initsink, mGraph);
         reader.run();
+        mGraph = new Graph((HashMap<Long, Node>) reader.getMainSink().getNodes());
     }
 }
